8b1323bc13f0514f35622a76acb69e21605b4793
Maria Ghorbani <ghorbanimoghaddam.m@gmail.com>
2022-10-27 18:35:37 +0300

    modifiers ( readonly, private and public ) use in constructor

--- public/main.js
+++ public/main.js
@@ -1,9 +1,9 @@
 "use strict";
 class Invoice {
-    constructor(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     format() {
         return `${this.client} ows ${this.amount} for ${this.details}`;
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
 class Invoice {
-    client: string;
-    details: string;
-    amount: number;
-
-    constructor( c: string, d: string, a: number) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
-    }
+    constructor(
+        readonly client: string,
+        private details: string,
+        public amount: number,
+    ) {}
 
     format() {
         return `${this.client} ows ${this.amount} for ${this.details}`
