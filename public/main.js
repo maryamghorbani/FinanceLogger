1193d7c83b2b312811a645b7f8a4aba35f359427
Maria Ghorbani <ghorbanimoghaddam.m@gmail.com>
2022-11-01 10:12:23 +0200

    create ul list

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 import { Invoice } from "./classes/Invoice.js";
 import { Payment } from "./classes/Payment.js";
+import { ListTemplate } from "./classes/ListTemplate";
 // let docOne: HasFormatter;
 // let docTwo: HasFormatter;
 //
@@ -23,6 +24,9 @@ const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -32,5 +36,5 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
